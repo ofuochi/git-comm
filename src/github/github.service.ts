282a00a84071c3c05ae8fcca5787cef3a6d69814
Fortune <fortuneochi@gmail.com>
2021-05-05 12:11:14 +0100

    feat: implemented service to query github commits

--- src/github/github.service.ts
+++ src/github/github.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { HttpService, Injectable } from '@nestjs/common';
+import { AxiosResponse } from 'axios';
+import { plainToClass } from 'class-transformer';
+import { map } from 'rxjs/operators';
+
+import { GithubCommitDto } from './dto/github-commit.dto';
+import { QueryGithubCommitDto } from './dto/query-commit.dto';
 
 @Injectable()
-export class GithubService {}
+export class GithubService {
+  constructor(private readonly httpService: HttpService) {}
+
+  getCommits(
+    owner: string,
+    repo: string,
+    query?: QueryGithubCommitDto,
+  ): Promise<GithubCommitDto[]> {
+    // transform response and map to DTO
+    const transformRes = map<AxiosResponse<any[]>, GithubCommitDto[]>(
+      ({ data }) => {
+        const transform = plainToClass(GithubCommitDto, data, {
+          enableImplicitConversion: true,
+          excludeExtraneousValues: true,
+        });
+        return transform;
+      },
+    );
+
+    return this.httpService
+      .get(`/repos/${owner}/${repo}/commits`, { params: query })
+      .pipe<GithubCommitDto[]>(transformRes)
+      .toPromise();
+  }
+}
