92605240d080645259bdc73972061e4acb26d1c8
Fortune <fortuneochi@gmail.com>
2021-05-05 11:18:25 +0100

    feat: setup github service module

--- src/github/github.module.ts
+++ src/github/github.module.ts
@@ -1,9 +1,25 @@
-import { Module } from '@nestjs/common';
+import { HttpModule, Module } from '@nestjs/common';
+
+import configuration from '../shared/config/env.config';
 import { GithubController } from './github.controller';
 import { GithubService } from './github.service';
 
 @Module({
+  imports: [
+    HttpModule.registerAsync({
+      useFactory: () => {
+        // initialize github configuration
+        const {
+          githubConfig: { accessHeader, baseUrl },
+        } = configuration();
+        return {
+          headers: { Accept: accessHeader },
+          baseURL: baseUrl,
+        };
+      },
+    }),
+  ],
+  providers: [GithubService],
   controllers: [GithubController],
-  providers: [GithubService]
 })
 export class GithubModule {}
